77a3282a000a4bdf72425cdc1b4a759ce625bf32
saile515 <elias.jorgensen2006@gmail.com>
2023-11-28 19:30:46 +0100

    add shader compilation

--- src/graphics/implementations/opengl/shader.cpp
+++ src/graphics/implementations/opengl/shader.cpp
@@ -21,5 +21,51 @@ int origo::Shader::load_shader(std::string vertex_path,
         return 1;
     }
 
+    // Compile shader
+    const char* vertex_source_c = vertex_source.value().c_str();
+    glShaderSource(vertex_shader_id, 1, &vertex_source_c, NULL);
+    glCompileShader(vertex_shader_id);
+
+    // Check if shader compiled
+    int vertex_result = GL_FALSE;
+    glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &vertex_result);
+
+    if (vertex_result != GL_TRUE) {
+        return 1;
+    };
+
+    // ^^^
+    const char* fragment_source_c = fragment_source.value().c_str();
+    glShaderSource(fragment_shader_id, 1, &fragment_source_c, NULL);
+    glCompileShader(fragment_shader_id);
+
+    // ^^^
+    int fragment_result = GL_FALSE;
+    glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &fragment_result);
+
+    if (fragment_result != GL_TRUE) {
+        return 1;
+    };
+
+    // Create shader program
+    _shader_id = glCreateProgram();
+    glAttachShader(_shader_id, vertex_shader_id);
+    glAttachShader(_shader_id, fragment_shader_id);
+    glLinkProgram(_shader_id);
+
+    // Check if program linked
+    int program_result = GL_FALSE;
+    glGetProgramiv(_shader_id, GL_LINK_STATUS, &program_result);
+
+    if (program_result != GL_TRUE) {
+        return 1;
+    }
+
+    // Delete shaders
+    glDetachShader(_shader_id, vertex_shader_id);
+    glDetachShader(_shader_id, fragment_shader_id);
+    glDeleteShader(vertex_shader_id);
+    glDeleteShader(fragment_shader_id);
+
     return 0;
 }
